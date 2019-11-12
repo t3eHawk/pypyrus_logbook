c3de6c37a4a16b19a1bbf4eb9bff44cd8e3930e7
t3eHawk <42487634+t3eHawk@users.noreply.github.com>
2019-11-12 12:15:42 +0300

    Changes in README and tests

--- test/test15.py
+++
@@ -1,7 +0,0 @@
-import pypyrus_logbook as logbook
-
-
-log = logbook.getlogger()
-
-for i in range(10):
-    log.info(f'The record number {i}')
--- test/test18.py
+++
@@ -1,7 +0,0 @@
-import pypyrus_logbook as logbook
-
-log = logbook.getlogger()
-
-log.sysinfo.add('-t', '--test', required=False, type=int)
-print(log.sysinfo)
-print(log.sysinfo.anons)
